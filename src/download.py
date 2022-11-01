3b1645e90d7746925b45dd44593076981a93234f
unknown <lewisdy@tcd.ie>
2022-11-01 12:19:49 +0000

    Breaking it down

--- src/download.py
+++ src/download.py
@@ -1,9 +1,24 @@
 import io
 import logging
+from xmlrpc.client import boolean
 import requests
 import zipfile
 
-def download_GTFS_zip(zip_url: str, directory: str = ".\\GTFS") -> str:
+def download_GTFS_zip(zip_url: str):
+    """Download a GTFS zip file at the specified url.
+
+    Arguments:
+        zip_url:    Url where the zipped folder is.
+    Returns:
+        Zipfile or None if unable to retrieve the zip file.
+    """
+    resp = requests.get(zip_url, stream=True)
+    if not resp.ok:
+        logging.error(f"Got invalid response {resp.status_code} from url {zip_url}")
+        return None
+    return zipfile.ZipFile(io.BytesIO(resp.content))
+
+def get_GTFS_files(zip_url: str, directory: str = ".\\GTFS") -> str:
     """Download and extract the GTFS files from a zip file at the
     specified url.
 
@@ -15,11 +30,9 @@ def download_GTFS_zip(zip_url: str, directory: str = ".\\GTFS") -> str:
         A string with where the folder is however if an error occurs
         it will return an empty string
     """
-    resp = requests.get(zip_url, stream=True)
-    if not resp.ok:
-        logging.error(f"Got invalid response {resp.status_code} from url {zip_url}")
+    zip_file = download_GTFS_zip(zip_url)
+    if zip_file == None:
         return ""
-    zip_file = zipfile.ZipFile(io.BytesIO(resp.content))
     logging.debug("Extracting zip file")
     zip_file.extractall(directory)
     logging.debug("Finished extracting zip file")
